fd30d686714ae48ed8ed5228762a0c92755ba03d
吴宏佳 <779493113@qq.com>
2020-04-25 22:38:54 +0800

    ..

--- src/views/ADGenerate.vue
+++ src/views/ADGenerate.vue
@@ -4,22 +4,25 @@
         <Row :gutter="16">
 
             <Col span="12">
-            <Input v-model="channelName"  placeholder="广告名称前缀（或是渠道名）" >
-            <Select v-model="platform" slot="prepend" style="width: 80px">
-                <Option value="adnet">优亮汇</Option>
-                <Option value="x5">TBS</Option>
-                <Option value="sougou">搜狗</Option>
+                <Input v-model="channelName" placeholder="广告名称前缀（或是渠道名）">
+                <Select v-model="platform" slot="prepend" style="width: 80px">
+                    <Option value="adnet">优亮汇</Option>
+                    <Option value="x5">TBS</Option>
+                    <Option value="sougou">搜狗</Option>
 
-            </Select>
+                </Select>
 
-            <Button slot="append" @click="prepareAction">预创建</Button>
+                <Button slot="append" @click="prepareAction">预创建</Button>
 
-            </Input>
+                </Input>
+                <Button :type="wsStatus" :loading="wsLoading" @click="wsAction">
+                    <span v-if="!wsLoading">{{wsStatus}}!</span>
+                    <span v-else>连接中...</span>
+                </Button>
             </Col>
             <Col span="12">
 
-                <ADLocation  v-model="adLocs"></ADLocation>
-
+                <ADLocation v-model="adLocs"></ADLocation>
 
             </Col>
 
@@ -27,10 +30,10 @@
         <Row>
             <Input type="textarea"
                    :rows="20"
-                   v-model="prepareStr" />
+                   v-model="prepareStr"/>
         </Row>
         <Row>
-            <Button  @click="createAction" >创建</Button>
+            <Button @click="createAction">创建</Button>
 
         </Row>
     </div>
@@ -38,6 +41,7 @@
 
 <script>
     import ADLocation from "../components/ADLocation";
+
     export default {
         name: "ADGenerate",
         components: {ADLocation},
@@ -45,46 +49,69 @@
             return {
                 ws: null,
                 platform: '',
-                channelName:'',
-                prepareStr:'',
-                adLocs: []
+                channelName: '',
+                prepareStr: '',
+                adLocs: [],
+                wsLoading: false,
+                wsStatus: 'dashed'
             }
         },
         methods: {
-            createAction(){},
-            send() {
-                let obj = {func: 'whj'};
+            createAction() {
+
+                if (this.wsStatus !== 'success') {
+                    this.$Message.warning('服务已经断开！请重连');
+                    return null;
+                }
+                this.send('create1');
+                this.send('create');
+
+            },
+            send(func) {
+                let obj = {func,body:'hello'};
                 this.ws.send(JSON.stringify(obj));
 
             },
-
-            prepareAction(){
+            wsAction() {
+                if (this.wsStatus !== 'success') {
+                    this.openWs();
+                }
+            },
+            prepareAction() {
                 if (!this.channelName) return;
                 this.prepareStr = "";
                 let prepareArr = [];
-                this.adLocs.forEach(obj=>{
-                    for (let num = obj.start ; num <= obj.stop; num++) {
-                        prepareArr.push( this.channelName + obj.text + num );
+                this.adLocs.forEach(obj => {
+                    for (let num = obj.start; num <= obj.stop; num++) {
+                        prepareArr.push(this.channelName + obj.text + num);
                     }
                 });
                 this.prepareStr = prepareArr.join('\n')
 
+            },
+            openWs() {
+                this.wsLoading = true;
+                this.ws = new WebSocket('ws://localhost:8765');
+                this.ws.onopen = () => {
+                    this.wsLoading = false;
+                    this.wsStatus = 'success'
+                };
+                this.ws.onclose = ev => {
+                    console.log(ev);
+                    this.wsStatus = 'warning'
+                };
+                this.ws.onerror = ev => {
+                    console.log(ev);
+                    this.wsStatus = 'error'
+                };
+                this.ws.onmessage = ev => {
+                    let obj = JSON.parse(ev.data);
+                };
             }
         },
         created() {
-            this.ws = new WebSocket('ws://localhost:8765');
-            this.ws.onopen = () => {
+            this.openWs();
 
-            };
-            this.ws.onmessage = ev => {
-                let obj = JSON.parse(ev.data);
-            };
-            this.ws.onclose = ev => {
-                console.log(ev);
-            };
-            this.ws.onerror = ev => {
-                console.log(ev);
-            };
         },
         destroyed() {
             this.ws.close();
@@ -93,7 +120,7 @@
 </script>
 
 <style scoped>
-.ADGenerate{
-    margin: 10px;
-}
+    .ADGenerate {
+        margin: 10px;
+    }
 </style>
