6d42370ab7661e910577376131a64d5039d82407
yuwu <769441745@qq.com>
2020-05-15 18:00:47 +0800

    预渲染配置中

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+
+// 3、相关配置
+// 预渲染配置：在webpack.prod.conf文件中加入
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+const path = require('path');
+
+
+
+
 module.exports = {
     publicPath: '',
     productionSourceMap: false,
@@ -22,6 +32,42 @@ module.exports = {
             }
         }
     },
+    configureWebpack: {
+        plugins: [
+            new PrerenderSPAPlugin({
+                staticDir: path.join(__dirname, 'dist'),
+                routes: [ '/', '/test' ],
 
-    lintOnSave: false
+                renderer: new Renderer({
+                    inject: {
+                        foo: 'bar'
+                    },
+                    headless: true,
+                    renderAfterDocumentEvent: 'render-event'
+                })
+            })
+        ]
+    }
+    // lintOnSave: false,
+    // pages: {
+    //     index: {
+    //         // page 的入口
+    //         entry: 'src/main.js',
+    //         // 模板来源
+    //         template: 'public/index.html',
+    //         // 在 dist/index.html 的输出
+    //         filename: 'index.html',
+    //         // 当使用 title 选项时，
+    //         // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+    //         title: 'Index Page',
+    //         // 在这个页面中包含的块，默认情况下会包含
+    //         // 提取出来的通用 chunk 和 vendor chunk。
+    //         chunks: ['chunk-vendors', 'chunk-common', 'index']
+    //     },
+    //     // 当使用只有入口的字符串格式时，
+    //     // 模板会被推导为 `public/subpage.html`
+    //     // 并且如果找不到的话，就回退到 `public/index.html`。
+    //     // 输出文件名会被推导为 `subpage.html`。
+    //     subpage: 'src/subpage/main.js'
+    // }
 };
